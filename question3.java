a878891a47d6b452943da844496685efa596693b
anshchawla521 <anshchawla521@gmail.com>
2022-10-11 00:22:09 +0530

    completed Question 3

---
+++ question3.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class question3 {
+    public static void main(String args[])
+    {
+        String[] my_array = {"ansh","chawla","yo yo","honey","singh","badhshah","kaplana chawla"};
+        Arrays.sort(my_array); // inbuilt sort function
+        System.out.println(Arrays.toString(my_array));   // inbuilt tostring  function for arrays
+        
+    }
+}
